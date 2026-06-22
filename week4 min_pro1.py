5f3746d74bbb33a80ae09dd4f4e242d36e386f0e
intiyaz786 <mumtazshaik363@gmail.com>
2026-06-22 16:56:44 +0530

    Add files via upload

---
+++ week4 min_pro1.py
@@ -0,0 +1,31 @@
+import csv
+
+print("Student Email List")
+print("-" * 30)
+
+with open("students.csv", "r") as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        name = row["Name"]
+        email = row["Email"]
+
+        subject = "Python Internship Update"
+
+        message = f"""
+Hello {name},
+
+Congratulations!
+
+You have been selected for the Python Internship Program.
+
+Best Regards,
+Training Team
+"""
+
+        print("To:", email)
+        print("Subject:", subject)
+        print(message)
+        print("-" * 30)
+
+print("All emails processed successfully!")
